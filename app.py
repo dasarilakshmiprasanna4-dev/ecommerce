0300c1e428ec8aea676b120d361f35ec6b25e762
dasarilakshmiprasanna4-dev <dasarilakshmiprasanna4@gmail.com>
2026-06-30 15:20:51 +0530

    FD

--- app.py
+++ app.py
@@ -53,11 +53,11 @@ app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 
 app.secret_key = "Code123"
 
-app.config["SESSION_COOKIE_SECURE"] = True
-# app.config["SESSION_COOKIE_SECURE"] = False
+# app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SECURE"] = False
 app.config["SESSION_COOKIE_HTTPONLY"] = True
-# app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+# app.config["SESSION_COOKIE_SAMESITE"] = "None"
 
 app.permanent_session_lifetime = timedelta(days=1)
 app.config['PREFERED_URL_SCHEME']='https'
